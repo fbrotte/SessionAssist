07006ebcbc45651e89b30324dc24c82eb9406b9a
Florian BROTTE <contact@fbrotte.fr>
2022-10-23 21:16:27 +0200

    Feat(integration): Create a components with style

---
+++ components/TitleHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="title-header">
+        <h1><slot/></h1>
+        <p v-if="paginate">{{ paginate }} / 3</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['paginate'])
+</script>
