046fe1a27911cf104f98233ca8ec5f4a9b0986d8
Florian BROTTE <contact@fbrotte.fr>
2022-10-23 20:55:18 +0200

    Feat(css): Implement sass with the first rule

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    title: 'Session Assist',
+    head: {
+        meta: [
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        ],
+    },
+    css: [
+    '~/assets/css/main.scss'
+    ],
 })
